8c66f91bb8f7128c2b95321795b9a434b12d5de2
Michael Lim <59800222+savon94@users.noreply.github.com>
2024-04-01 15:17:50 +0900

    Update admin.ts

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -3,6 +3,7 @@ import { getFirestore } from 'firebase-admin/firestore';
 import { FB_CLIENT_EMAIL, FB_PRIVATE_KEY, FB_PROJECT_ID, FB_SERVICE_ACCOUNT } from '$env/static/private'
 import pkg from 'firebase-admin';
 
+
 try {
     pkg.initializeApp({
         credential: pkg.credential.cert({
